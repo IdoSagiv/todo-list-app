69f3d3148f29d51504997b600054bd1b5bfb8c81
Ido Sagiv <ido.sagiv@mail.huji.ac.il>
2021-05-13 18:18:03 +0300

    fix indentation in tests

--- app/src/test/java/exercise/android/reemh/todo_items/TodoItemsHolderImplTest.java
+++ app/src/test/java/exercise/android/reemh/todo_items/TodoItemsHolderImplTest.java
@@ -5,18 +5,18 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 public class TodoItemsHolderImplTest extends TestCase {
-  @Test
-  public void when_addingTodoItem_then_callingListShouldHaveThisItem(){
-    // setup
-    TodoItemsHolderImpl holderUnderTest = new TodoItemsHolderImpl();
-    assertEquals(0, holderUnderTest.getCurrentItems().size());
+    @Test
+    public void when_addingTodoItem_then_callingListShouldHaveThisItem() {
+        // setup
+        TodoItemsHolderImpl holderUnderTest = new TodoItemsHolderImpl();
+        assertEquals(0, holderUnderTest.getCurrentItems().size());
 
-    // test
-    holderUnderTest.addNewInProgressItem("do shopping");
+        // test
+        holderUnderTest.addNewInProgressItem("do shopping");
 
-    // verify
-    assertEquals(1, holderUnderTest.getCurrentItems().size());
-  }
+        // verify
+        assertEquals(1, holderUnderTest.getCurrentItems().size());
+    }
 
-  // TODO: add at least 10 more tests to verify correct behavior of your implementation of `TodoItemsHolderImpl` class
+    // TODO: add at least 10 more tests to verify correct behavior of your implementation of `TodoItemsHolderImpl` class
 }
